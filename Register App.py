20dcb1adb7685f6783c3aefbaf6d345e6618920e
Yusuf Yüksel <129797259+yusufsyuksel@users.noreply.github.com>
2024-05-31 15:23:49 +0300

    Update Register App.py

--- Register App.py
+++ Register App.py
@@ -77,7 +77,7 @@ while True:
         repository.register(user)
     elif secim == '2':
         if repository.isLoggedIn:
-            print('zaten girdiş yapıldı')
+            print('zaten giriş yapıldı')
         else:
             username = input('username: ')
             password = input('password: ')
